0ef1546bf466701226cbce3ff12965fa7f443901
Pankaj Garg <pankaj.garg@oodles.io>
2023-06-26 14:07:10 +0530

    First Commit

---
+++ src/test/java/io/pankaj/SpringBootAWS/SpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pankaj.SpringBootAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
